11ebdca6c4d310a30ab9b3906a3c588eb7729073
Evgeny Ukhanov <mrlsd@ya.ru>
2017-08-21 10:32:41 +0300

    Added 404 Error handling and added Templates fairing

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
 
 use std::path::{Path, PathBuf};
+use std::collections::HashMap;
+use rocket::Request;
 use rocket::response::{NamedFile};
+use rocket_contrib::Template;
 
 #[get("/")]
 fn hello() -> &'static str {
@@ -17,6 +21,17 @@ fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+#[error(404)]
+fn error_not_found(_: &Request) -> Template {
+    let mut map: HashMap<&str, &str> = HashMap::new();
+    // map.insert("path", req.uri().as_str());
+    Template::render("error/404", &map)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![hello])
+        .attach(Template::fairing())
+        .catch(errors![error_not_found])
+        .launch();
 }
