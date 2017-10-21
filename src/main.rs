d7c22987dd71a6a719a1e9ef5886241830708f51
Evgeny Ukhanov <mrlsd@ya.ru>
2017-10-22 00:53:48 +0300

    Added login page sample

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,12 @@ fn hello() -> &'static str {
     "rocket.rs"
 }
 
+#[get("/login")]
+fn login() -> Template {
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("backend/login/login", &context)
+}
+
 // Static files handler
 #[get("/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
@@ -23,21 +29,21 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 
 #[error(404)]
 fn error_not_found(_: &Request) -> Template {
-    let mut map: HashMap<&str, &str> = HashMap::new();
-    // map.insert("path", req.uri().as_str());
-    Template::render("error/404", &map)
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("errors/404", &context)
 }
 
 #[error(500)]
 fn error_internal(_: &Request) -> Template {
-    let mut map: HashMap<&str, &str> = HashMap::new();
-    // map.insert("path", req.uri().as_str());
-    Template::render("error/500", &map)
+    let context: HashMap<&str, &str> = HashMap::new();
+    Template::render("errors/500", &context)
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![hello, files])
+        .mount("/", routes![
+            hello, files, login
+        ])
         .attach(Template::fairing())
         .catch(errors![error_not_found, error_internal])
         .launch();
